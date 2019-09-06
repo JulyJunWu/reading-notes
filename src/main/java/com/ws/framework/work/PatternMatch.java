693b9c238ae4168c43a8a75355fae27b2f998663
qjunwu <qjunwu@linewell.com>
2019-09-06 18:25:42 +0800

    linux命令3

--- src/main/java/com/ws/framework/work/PatternMatch.java
+++ src/main/java/com/ws/framework/work/PatternMatch.java
@@ -24,4 +24,18 @@ public class PatternMatch {
         System.out.println(matches);
 
     }
+
+    /**
+     * 验证字符串是否为全英文+数字
+     */
+    @Test
+    public void isEnglishAndNumber() {
+
+        String str = "retretHHH";
+
+        boolean matches = Pattern.matches("[0-9a-zA-Z]+", str);
+
+        System.out.println(matches);
+
+    }
 }
