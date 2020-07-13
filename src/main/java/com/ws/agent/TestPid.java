b37c8a9a095231821f93d14fe877d0a913eae3e2
JulyJunWu <596889735@qq.com>
2020-07-13 23:31:41 +0800

    ManagementFactory.getRuntimeMXBean()获取PID

---
+++ src/main/java/com/ws/agent/TestPid.java
@@ -0,0 +1,26 @@
+package com.ws.agent;
+
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @Description: 测试从Java代码中获取自身程序的PID
+ * @Author: JulyJunWu
+ * @Date: 2020/7/13 23:26
+ */
+public class TestPid {
+
+    public static void main(String[] args) {
+
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        System.out.println(name);
+
+        long longPid = PidUtils.currentLongPid();
+        String currentPid = PidUtils.currentPid();
+        System.out.println(longPid);
+        System.out.println(currentPid);
+
+        LockSupport.park();
+    }
+
+}
